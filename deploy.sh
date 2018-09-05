1064d160836f07ecbbfee19cf20b5ed4ebf69fc3
Chau Nguyen <remynguyen96@gmail.com>
2018-09-06 05:06:05 +0700

    Group commit in one

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
+BIRed='\033[1;91m'
+BIGreen='\033[1;92m'
+BIBlue='\033[1;94m'
+BIYellow='\033[1;93m'
+UYellow='\033[4;33m'
+
+function messageTxt {
+    echo -e "$1--------------------------\n$message\n--------------------------\n"
+}
+
 git status
 
 yarn version --patch
+message="Bump version for application success"
+messageTxt ${BIGreen}
 
 git add -A
+message="Git add all file in project success"
+messageTxt ${BIBlue}
 
-git commit -m "$(curl -s whatthecommit.com/index.txt)"
+commit=$(curl -s whatthecommit.com/index.txt);
+git commit -m "${commit}"
+message="Commit: ${BIYellow}${commit}"
+messageTxt ${UYellow}
 
 git push origin
-
-
-
-
